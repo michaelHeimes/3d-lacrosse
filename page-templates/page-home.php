61cc1e6e1b335fcbe7234a9cd786be1fdfd1853c
Michael Heimes <michael@cairndigitalmedia.com>
2022-07-25 20:45:29 -0400

    dev

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -15,19 +15,48 @@ get_header();
 
 				<main id="primary" class="site-main">
 			
-					<?php
-					while ( have_posts() ) :
-						the_post();
-			
-						get_template_part( 'template-parts/content', 'page' );
-			
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-			
-					endwhile; // End of the loop.
-					?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							
+						</header><!-- .entry-header -->
+					
+					
+						<div class="entry-content">
+							<?php
+							the_content();
+					
+							wp_link_pages(
+								array(
+									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'lacrosse-3d' ),
+									'after'  => '</div>',
+								)
+							);
+							?>
+						</div><!-- .entry-content -->
+					
+						<?php if ( get_edit_post_link() ) : ?>
+							<footer class="entry-footer">
+								<?php
+								edit_post_link(
+									sprintf(
+										wp_kses(
+											/* translators: %s: Name of current post. Only visible to screen readers */
+											__( 'Edit <span class="screen-reader-text">%s</span>', 'lacrosse-3d' ),
+											array(
+												'span' => array(
+													'class' => array(),
+												),
+											)
+										),
+										wp_kses_post( get_the_title() )
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+								?>
+							</footer><!-- .entry-footer -->
+						<?php endif; ?>
+					</article><!-- #post-<?php the_ID(); ?> -->
 			
 				</main><!-- #main -->
 					
